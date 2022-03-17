e5268a3c6062d3a53c257d5ae241bcec777207d4
ykguleriu <yguler@ogr.iu.edu.tr>
2022-03-17 12:23:48 +0300

    first changes

--- lib/bootstrap-client.ts
+++ lib/bootstrap-client.ts
@@ -1,13 +1,22 @@
 export function bootstrap() {
   console.log(`
+YYYYYYY       YYYYYYYKKKKKKKKK    KKKKKKK       GGGGGGGGGGGGG
+Y:::::Y       Y:::::YK:::::::K    K:::::K    GGG::::::::::::G
+Y:::::Y       Y:::::YK:::::::K    K:::::K  GG:::::::::::::::G
+Y::::::Y     Y::::::YK:::::::K   K::::::K G:::::GGGGGGGG::::G
+YYY:::::Y   Y:::::YYYKK::::::K  K:::::KKKG:::::G       GGGGGG
+   Y:::::Y Y:::::Y     K:::::K K:::::K  G:::::G
+    Y:::::Y:::::Y      K::::::K:::::K   G:::::G
+     Y:::::::::Y       K:::::::::::K    G:::::G    GGGGGGGGGG
+      Y:::::::Y        K:::::::::::K    G:::::G    G::::::::G
+       Y:::::Y         K::::::K:::::K   G:::::G    GGGGG::::G
+       Y:::::Y         K:::::K K:::::K  G:::::G        G::::G
+       Y:::::Y       KK::::::K  K:::::KKKG:::::G       G::::G
+       Y:::::Y       K:::::::K   K::::::K G:::::GGGGGGGG::::G
+    YYYY:::::YYYY    K:::::::K    K:::::K  GG:::::::::::::::G
+    Y:::::::::::Y    K:::::::K    K:::::K    GGG::::::GGG:::G
+    YYYYYYYYYYYYY    KKKKKKKKK    KKKKKKK       GGGGGG   GGGG
 
-████████╗██████╗  █████╗ ███╗   ██╗███████╗██╗████████╗██╗██╗   ██╗███████╗    ██████╗ ███████╗
-╚══██╔══╝██╔══██╗██╔══██╗████╗  ██║██╔════╝██║╚══██╔══╝██║██║   ██║██╔════╝    ██╔══██╗██╔════╝
-   ██║   ██████╔╝███████║██╔██╗ ██║███████╗██║   ██║   ██║██║   ██║█████╗      ██████╔╝███████╗
-   ██║   ██╔══██╗██╔══██║██║╚██╗██║╚════██║██║   ██║   ██║╚██╗ ██╔╝██╔══╝      ██╔══██╗╚════██║
-   ██║   ██║  ██║██║  ██║██║ ╚████║███████║██║   ██║   ██║ ╚████╔╝ ███████╗    ██████╔╝███████║
-   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝╚═╝   ╚═╝   ╚═╝  ╚═══╝  ╚══════╝    ╚═════╝ ╚══════╝
-                                                                                               
-   This site is built using Notion, Next.js, and https://github.com/NotionX/react-notion-x.
+"These aren't the droids you are looking for"
 `)
 }
